3257d8204e5b7a11ffa52a6bbc6b07badfd4cf41
Lakshan Shridhar <169003973+LakshanShridhar@users.noreply.github.com>
2025-06-24 16:15:05 -0700

    Update test_signup.py

--- test_signup.py
+++ test_signup.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-from signup import app  # Import your signup FastAPI app
+from signup import app  # Import the signup FastAPI app
 
 client = TestClient(app)
 
